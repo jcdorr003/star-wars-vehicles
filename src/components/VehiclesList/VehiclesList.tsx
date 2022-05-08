34eb20ad473a74912c57a1ad653c5997885460b1
JC Dorr <jc@vidmob.com>
2022-05-08 00:15:42 -0400

    initial setup, added folders and files, and started creating app structure.

---
+++ src/components/VehiclesList/VehiclesList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './VehiclesList.scss';
+
+const VehiclesList = () => {
+  return (
+    <div className="vehicles-list-container">VehiclesList</div>
+  )
+}
+
+export default VehiclesList
