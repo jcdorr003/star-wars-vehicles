dd2f4aa81c0911d6d9b523a51048fa1f6771b325
JC Dorr <jc@vidmob.com>
2022-05-10 20:01:00 -0400

    more styling

--- src/components/FilmDetails/FilmDetails.tsx
+++ src/components/FilmDetails/FilmDetails.tsx
@@ -1,22 +1,29 @@
 import React from "react"
 import { IFilmDetails } from "../../types";
 import './FilmDetails.scss';
+import posterImg from '../../assets/images/SW_E1_poster.webp';
 
 interface FilmDetailsProps {
   film: IFilmDetails
 }
 
 const FilmDetails: React.FC<FilmDetailsProps> = ({film}) => {
+
+  console.log('posterImg :>> ', posterImg);
   return (
     <div className="film-details-container">
       {film.title && (
         <>
         <h1 className="film-details-header">{film.title}</h1>
         <div className="film-details">
+          <div className="poster-container">
+            <img src={posterImg} alt="" className="poster" />
+          </div>
           <p className="director">Director: {film.director}</p>
           <p className="producer">Producer: {film.producer}</p>
           <p className="producer">Release Date: {film.release_date}</p>
           <p className="producer">Opening Crawl: {film.opening_crawl}</p>
+          
         </div>
         </>
         )
